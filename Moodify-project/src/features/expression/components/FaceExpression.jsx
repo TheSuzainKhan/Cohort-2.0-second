eff36c88972a6b82aa46ecae33330170ad0c6574
Suzain Khan <its.suzainkhan@gmail.com>
2026-03-27 21:35:57 +0530

    feat: add SVG assets for React and Vite logos
    feat: implement FaceExpression component for detecting facial expressions
    
    style: add global styles and CSS variables for theming
    
    chore: set up main entry point for React application
    
    chore: configure Vite for React project

---
+++ Moodify-project/src/features/expression/components/FaceExpression.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import {
+    FaceLandmarker,
+    FilesetResolver
+} from "@mediapipe/tasks-vision";
+
+export default function FaceExpression() {
+    const videoRef = useRef(null);
+    const landmarkerRef = useRef(null);
+    const animationRef = useRef(null);
+    let stream;
+
+    const [ expression, setExpression ] = useState("Detecting...");
+
+    const init = async () => {
+    };
+
+    const detect = () => {
+    };
+
+    useEffect(() => {
+        
+
+        init();
+
+        return () => {
+            if (animationRef.current) {
+                cancelAnimationFrame(animationRef.current);
+            }
+
+            if (landmarkerRef.current) {
+                landmarkerRef.current.close();
+            }
+
+            if (videoRef.current?.srcObject) {
+                videoRef.current.srcObject
+                    .getTracks()
+                    .forEach((track) => track.stop());
+            }
+        };
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <video
+                ref={videoRef}
+                style={{ width: "400px", borderRadius: "12px" }}
+                playsInline
+            />
+            <h2>{expression}</h2>
+            <button onClick={detect} >Detect expression</button>
+        </div>
+    );
+}
